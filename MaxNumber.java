cf75a872b39a3e3819a8584edfeded41fb848cce
Mohan Gowda R <mohanrmoni54@gmail.com>
2026-04-23 00:11:24 +0530

    Create MaxNumber.java

---
+++ MaxNumber.java
@@ -0,0 +1,24 @@
+public class MaxNumber {
+    public static void main(String[] args) {
+        int[] arr = {12, 45, 7, 89, 34};
+        int max = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Maximum number is: " + max);
+    }
+}
+
+FROM openjdk:17
+
+WORKDIR /app
+
+COPY MaxNumber.java .
+
+RUN javac MaxNumber.java
+
+CMD ["java", "MaxNumber"]
